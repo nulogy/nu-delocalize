d1070a07a73f57ef4bcb1c5e1c7f846d4b58046d
Bryce Kwan <brycek@nulogy.com>
2015-03-26 17:31:02 -0400

    redo patch from 0.3.1

--- lib/delocalize/rails_ext/active_record.rb
+++ lib/delocalize/rails_ext/active_record.rb
@@ -1,25 +1,3 @@
-=begin
-ActiveRecord::Base.instance_eval do
-  def define_method_attribute=(attr_name)
-    if create_time_zone_conversion_attribute?(attr_name, columns_hash[attr_name])
-      method_body, line = <<-EOV, __LINE__ + 1
-        def #{attr_name}=(original_time)
-          time = original_time
-          unless time.acts_like?(:time)
-            time = time.is_a?(String) ? (I18n.delocalization_enabled? ? Time.zone.parse_localized(time) : Time.zone.parse(time)) : time.to_time rescue time
-          end
-          time = time.in_time_zone rescue nil if time
-          write_attribute(:#{attr_name}, original_time)
-          @attributes_cache["#{attr_name}"] = time
-        end
-      EOV
-      generated_attribute_methods.module_eval(method_body, __FILE__, line)
-    else
-      super
-    end
-  end
-=end
-
 if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('4.0.0.beta')
   require 'delocalize/rails_ext/active_record_rails4'
 else
