afa681e35f492dfab29b16ab16efc2a354e6fd52
Jason Schweier <jasons@nulogy.com>
2017-07-17 15:28:26 -0400

    revert railtie to pre-1.0 behaviour

--- lib/delocalize/railtie.rb
+++ lib/delocalize/railtie.rb
@@ -1,15 +1,23 @@
 module Delocalize
   class Railtie < Rails::Railtie
     initializer "delocalize" do |app|
-      ActiveSupport.on_load :action_controller do
-        # By default, we use ActionController::Parameters provided by strong_parameters. If it's
-        # not there, we fall back to our own replacement (Delocalize::Parameters).
-        if defined?(ActionController::Parameters)
-          ActionController::Parameters.send(:include, Delocalize::ParameterDelocalizing)
+      ActiveSupport.on_load :active_record do
+        if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('4.0.0.beta')
+          require 'delocalize/rails_ext/active_record_rails4'
         else
-          ActionController::Base.send(:include, Delocalize::DelocalizableParameters)
+          require 'delocalize/rails_ext/active_record_rails3'
         end
       end
+
+      ActiveSupport.on_load :action_view do
+        if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('4.0.0.beta')
+          require 'delocalize/rails_ext/action_view_rails4'
+        else
+          require 'delocalize/rails_ext/action_view_rails3'
+        end
+      end
+
+      require 'delocalize/rails_ext/time_zone'
     end
   end
 end
