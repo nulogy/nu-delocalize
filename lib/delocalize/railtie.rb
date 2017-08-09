b788808158148d7ae0c2189da3d888fe96ad75b6
Jason Schweier <jasons@nulogy.com>
2017-08-09 17:49:44 -0400

    explicitly only support Rails 4.x

--- lib/delocalize/railtie.rb
+++ lib/delocalize/railtie.rb
@@ -2,20 +2,28 @@ module Delocalize
   class Railtie < Rails::Railtie
     initializer "delocalize" do |app|
       ActiveSupport.on_load :active_record do
-        if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('4.0.0.beta')
-          require 'delocalize/rails_ext/active_record_rails4'
+        ar_version = ActiveRecord::VERSION::STRING
+
+        if Delocalize::Railtie.between_versions?(ar_version, "4.0.0.beta", "5.0.0.beta")
+          require "delocalize/rails_ext/active_record_rails4"
         else
-          require 'delocalize/rails_ext/active_record_rails3'
+          raise "Unsupported version of ActiveRecord: #{ar_version}"
         end
       end
 
       ActiveSupport.on_load :action_view do
-        if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('4.0.0.beta')
-          require 'delocalize/rails_ext/action_view_rails4'
+        av_version = ActionView::VERSION::STRING
+
+        if Delocalize::Railtie.between_versions?(av_version, "4.0.0.beta", "5.0.0.beta")
+          require "delocalize/rails_ext/action_view_rails4"
         else
-          require 'delocalize/rails_ext/action_view_rails3'
+          raise "Unsupported version of ActionView: #{av_version}"
         end
       end
+    end
+
+    def self.between_versions?(version, minimum_version, exclusive_maximum_version)
+      (Gem::Version.new(minimum_version)...Gem::Version.new(exclusive_maximum_version)).cover?(Gem::Version.new(minimum_version))
     end
   end
 end
