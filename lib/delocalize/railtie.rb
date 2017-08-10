cf5c1091b1a01a5036c3f18eb8e6681ce5962174
Jason Schweier <jasons@nulogy.com>
2017-08-10 10:50:25 -0400

    simplify version checking

--- lib/delocalize/railtie.rb
+++ lib/delocalize/railtie.rb
@@ -2,28 +2,20 @@ module Delocalize
   class Railtie < Rails::Railtie
     initializer "delocalize" do |app|
       ActiveSupport.on_load :active_record do
-        ar_version = ActiveRecord::VERSION::STRING
-
-        if Delocalize::Railtie.between_versions?(ar_version, "4.0.0.beta", "5.0.0.beta")
+        if ActiveRecord::VERSION::MAJOR == 4
           require "delocalize/rails_ext/active_record_rails4"
         else
-          raise "Unsupported version of ActiveRecord: #{ar_version}"
+          raise "Unsupported version of ActiveRecord: #{ActiveRecord::VERSION::STRING}"
         end
       end
 
       ActiveSupport.on_load :action_view do
-        av_version = ActionView::VERSION::STRING
-
-        if Delocalize::Railtie.between_versions?(av_version, "4.0.0.beta", "5.0.0.beta")
+        if ActionView::VERSION::MAJOR == 4
           require "delocalize/rails_ext/action_view_rails4"
         else
-          raise "Unsupported version of ActionView: #{av_version}"
+          raise "Unsupported version of ActionView: #{ActionView::VERSION::STRING}"
         end
       end
-    end
-
-    def self.between_versions?(version, minimum_version, exclusive_maximum_version)
-      (Gem::Version.new(minimum_version)...Gem::Version.new(exclusive_maximum_version)).cover?(Gem::Version.new(minimum_version))
     end
   end
 end
